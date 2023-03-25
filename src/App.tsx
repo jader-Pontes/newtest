79226137e52bbc9ea4f8ae7e44ebcacdfd9f053d
Jader <jaderluiz01@gmail.com>
2023-03-25 13:33:12 -0300

    Site funcionando e crindo o Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+//css
+import './App'
+
+//Context
+import {AuthProvider} from './context/Authcontext'
+
+//Components
+import Navbar from './components/Navbar';
+import Footer from "./components/Footer";
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Search from "./pages/Search";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/DashBoard";
+import Post from "./pages/PostBlog"
+
 
 function App() {
+
+  const [user,setUser]=useState<Boolean>(false)
+  const [password, setPassword] = useState<string>('');
+  const [email,setEmail]=useState<string>('')
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider>  
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/posts/:id" element={<Post />} />
+              <Route path="/search" element={<Search />} />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/register"
+                element={!user ? <Register /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/dashboard"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>     
     </div>
   );
 }
